2f0ab657e15649dc981ad499e908f780ad6d91ff
Santhosh <Santhosh@unknow>
2024-11-27 22:42:26 +0530

    First Version

---
+++ mygitmoviesystem/src/main/java/com/infosys/mygitmoviesystem/MyController.java
@@ -0,0 +1,18 @@
+package com.infosys.mygitmoviesystem;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class MyController {
+  
+	@GetMapping("/index")
+	public ModelAndView showIndexPage() {
+		return new ModelAndView("index");
+	}
+	@GetMapping("home")
+	public ModelAndView showHomePage() {
+		return new ModelAndView("home");
+	}
+}
